90afea3559bf7d4361cb502e59494b195f71a145
Jakub Gurgacz <37517798+zuber966@users.noreply.github.com>
2020-07-24 09:58:38 +0200

    switch to more appropriate class names

--- src/main/java/com/gurgaczj/jmaker/security/userdetails/AccountDetailsService.java
+++ src/main/java/com/gurgaczj/jmaker/security/userdetails/AccountDetailsService.java
@@ -1,6 +1,5 @@
 package com.gurgaczj.jmaker.security.userdetails;
 
-import com.gurgaczj.jmaker.exception.UserNotFoundException;
 import com.gurgaczj.jmaker.repository.AccountRepository;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -8,17 +7,17 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 @Service
-public class ApplicationUserDetailsService implements ReactiveUserDetailsService {
+public class AccountDetailsService implements ReactiveUserDetailsService {
 
     private final AccountRepository accountRepository;
 
-    public ApplicationUserDetailsService(AccountRepository accountRepository) {
+    public AccountDetailsService(AccountRepository accountRepository) {
         this.accountRepository = accountRepository;
     }
 
     @Override
     public Mono<UserDetails> findByUsername(String username) {
         return accountRepository.findByUsername(username)
-                .flatMap(account -> Mono.just(new ApplicationUserDetails(account)));
+                .flatMap(account -> Mono.just(new AccountDetails(account)));
     }
 }
